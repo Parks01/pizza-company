672e15f659d551f1e6bf50b02ffd8135c3957a1c
Parks01 <parksdrew01@gmail.com>
2017-07-26 12:08:11 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,36 @@
 //BUSINESS LOGIC
 
+
+function Dog(dogName,age,size,gender,dogImage,shelter,isAvailable)
+{
+	this.dogName = dogName;
+	this.age = age;
+	this.size = size;
+	this.gender = gender;
+	this.shelter = shelter;
+	this.dogImage = dogImage;
+	this.isAvailable = isAvailable;
+}
+var dogs = [];
+
+function fillDogsData()
+{
+	var newDog1 = new Dog("Jax", "adult", "medium" , "male", "img/jax.jpg",
+	"https://www.allpaws.com/adopt-a-dog/australian-cattle-dog-blue-heeler/6482237", true );
+	dogs[0] = newDog1;
+	var newDog2 = new Dog("Ellie", "adult", "small" , "female", "img/ellie.jpg", "https://www.allpaws.com/adopt-a-dog/shih-tzu/6644799" , true);
+	dogs[1] = newDog2;
+	var newDog3 = new Dog("Gunther", "adult", "small" , "male", "img/gunther.jpg",
+	"https://www.allpaws.com/adopt-a-dog/maltese/6405800" , true);
+	dogs[2] = newDog3;
+	var newDog4 = new Dog("Orion", "puppy", "medium" , "male", "img/orion.jpg" , "https://www.allpaws.com/adopt-a-dog/terrier/6070901", true);
+	dogs[3] = newDog4;
+//
+
+
+
+
+
 var custmerOrder = 0;
 var orderCalculation = 0;
 
